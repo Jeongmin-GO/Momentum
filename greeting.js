4f63e0415caa9403e752d16434cbca3f12e34c07
jeongmin97 <go.jminn@gmail.com>
2020-02-01 17:04:07 +0900

    greeting(save value)

--- greeting.js
+++ greeting.js
@@ -2,8 +2,25 @@ const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser", // local storage의 key값
+const USER_LS = "currentUser", // local storage의 기본key값
     SHOWING_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault(); //폼을 제출하면 자동으로 없어지는 기능 없앰
+    const currentValue = input.value;
+    paintGreeting(currentValue); //방금 제출한 입력값을 value에 넣음
+    saveName(currentValue);
+}
+// user가 없을 때 폼을 띄움
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+
+}
 
 //currentUser가 존재할 경우 value값을 색칠하는 함수
 function paintGreeting(text) {
@@ -16,6 +33,7 @@ function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
         // local storage에 user가 없을 때
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
