d6d558b9f38cc2eb80ac9f7af89ede4a57adee72
jeongmin97 <go.jminn@gmail.com>
2020-02-01 16:01:16 +0900

    Greeting

--- greeting.js
+++ greeting.js
@@ -1,8 +1,26 @@
 const form = document.querySelector(".js-form"),
-    input = form.querySelector("input");
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
 
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
 
-function init() {
-
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`
 }
+
+function loadNam() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+        // she is not
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+function init() {
+    loadName();
+}
+
 init();
