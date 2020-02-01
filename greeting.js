4f1e3ac69d6646a2a2f6b34849a6d088ff6fa118
jeongmin97 <go.jminn@gmail.com>
2020-02-01 16:25:16 +0900

    Greeting.js

--- greeting.js
+++ greeting.js
@@ -2,19 +2,20 @@ const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser",
+const USER_LS = "currentUser", // local storage의 key값
     SHOWING_CN = "showing";
 
+//currentUser가 존재할 경우 value값을 색칠하는 함수
 function paintGreeting(text) {
-    form.classList.remove(SHOWING_CN);
+    form.classList.remove(SHOWING_CN); // form is hidden
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`
+    greeting.innerText = `Hello ${text}`;
 }
 
-function loadNam() {
+function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
-        // she is not
+        // local storage에 user가 없을 때
     } else {
         paintGreeting(currentUser);
     }
