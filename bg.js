d143b9ebe82c858c3e19beb69eeffca527883fde
jeongmin97 <go.jminn@gmail.com>
2020-02-06 19:25:14 +0900

    BackgroundImage

---
+++ bg.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+
+const IMG_NUM = 4;
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`; //랜덤값이 0을 줄 수 있기 때문에 +1
+    image.classList.add("bgImage");
+    body.prepend(image);
+
+}
+
+function genRandom() {
+    const number = Math.floor(Math.random() * IMG_NUM);
+    return number;
+}
+
+function init() {
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+init();
