3692bacbcaf1a63b463b885899cd71964fc60f4f
jeongmin97 <go.jminn@gmail.com>
2020-02-10 02:45:15 +0900

    Location weather

---
+++ weather.js
@@ -0,0 +1,37 @@
+const COORDS = "coords";
+const API_KEY = "fe217f5d6247bbe8a435bc63dbc3fc20";
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+    console.log("cant access geo location");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCords = localStorage.getItem(COORDS);
+    if (loadedCords === null) {
+        askForCoords();
+    } else {
+        //getWeather
+    }
+}
+function init() {
+    loadCoords();
+}
+init();
